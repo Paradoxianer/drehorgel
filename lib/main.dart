96bf26f3397ba64f0e87ae187a50ca2dac5b65a2
paradoxon <blablabla!1865Atlassian>
2021-11-29 20:39:12 +0100

    Prepared taking Screenshot

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-import 'package:path_provider/path_provider.dart';
 import './the_crank.dart';
 import './donation_button.dart';
 
@@ -11,6 +12,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -26,54 +28,60 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   final String title;
+  GlobalKey previewContainer = new GlobalKey();
+
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          // TODO: add the Money Counter
-          title: Text(title),
-        ),
-        body: Center(
-          child: Stack(
-            children: <Widget>[
-              Image.asset('assets/images/drehorgel.png'),
-              FractionalTranslation(
-                  // TODO: Fix the position of the Crank according to the size of the Pipe Organ
-                  translation: Offset(0.5, 0.0),
-                  child: TheCrank())
-            ],
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                FloatingActionButton(
-                  onPressed: shareScreenshot(),
-                  icon: Icon(Icons.share)
+    return
+      RepaintBoundary(
+          key: previewContainer,
+          child: Scaffold(
+              appBar: AppBar(
+                // TODO: add the Money Counter
+                title: Text(title),
+              ),
+              body: Center(
+                child: Stack(
+                  children: <Widget>[
+                    Image.asset('assets/images/drehorgel.png'),
+                    /*FractionalTranslation(
+                      // TODO: Fix the position of the Crank according to the size of the Pipe Organ
+                        translation: Offset(0.5, 0.0),
+                        child: TheCrank())*/
+                    TheCrank()
+                  ],
                 ),
-                DonationButton()
-              ]
+              ),
+              bottomNavigationBar: BottomAppBar(
+                elevation: 0.0,
+                  child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        /*FloatingActionButton(
+                            onPressed: () {
+                              shareScreenshot();
+                            },
+                            child: Icon(Icons.share)
+                        ),*/
+                        DonationButton()
+                      ]
+                  )
+              )
           )
-        )
-
-        );
+      );
   }
-  
 
-
-   takeScreenShot() async{
-    RenderRepaintBoundary boundary = previewContainer.currentContext.findRenderObject();
-    ui.Image image = await boundary.toImage();
-    final directory = (await getApplicationDocumentsDirectory()).path;
-    ByteData byteData = await image.toByteData(format: ui.ImageByteFormat.png);
-    Uint8List pngBytes = byteData.buffer.asUint8List();
-    print(pngBytes);
-    File imgFile =new File('$directory/screenshot.png');
-        imgFile.writeAsBytes(pngBytes);
+  takeScreenShot() async {
+    BuildContext? cC = previewContainer.currentContext;
+    if (cC != null) {
+      RenderRepaintBoundary boundary = cC.findRenderObject() as RenderRepaintBoundary;
+      ui.Image image = await boundary.toImage();
+      ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+      return byteData;
+    }
   }
 
   Future shareScreenshot() async {
@@ -81,9 +89,11 @@ class MyHomePage extends StatelessWidget {
       await takeScreenShot();
       debugPrint("now we can share the screenshot");
       /*await EsysFlutterShare.shareImage(
-          'Game.png', bytes, 'Save The World');
-    } catch (e) {
+          'Game.png', bytes, 'Save The World');*/
+    }
+    catch (e) {
       print('error: $e');
-    }*/
+    }
+
   }
 }
