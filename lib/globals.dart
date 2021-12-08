eb966cd1e551c18eb2e6f23f77aab4598dcc920e
paradoxon <blablabla!1865Atlassian>
2021-12-08 22:49:52 +0100

    added global vars for calculation Position and Scale of the organ, officer and the donation bucket.

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/cupertino.dart';
 
+Size backgroundSize = Size(1280,720);
+Offset orgelOffset = Offset(5, -20);
 Size orgelSize  =  Size(794,559);
-Offset crankPoint = Offset(550, 459);
+Offset crankOffset = Offset(550, 459);
 Size crankSize = Size(500,500);
+Offset topfOffset = Offset(100,100);
+Size topfSize = Size(100,500);
+Offset salutisOffset = Offset(100,100);
+Size salutisSize = Size(100,500);
