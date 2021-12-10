d848537cf5adc41eae6e0d794a38812ea3ec2ccf
paradoxon <blablabla!1865Atlassian>
2021-12-10 06:52:42 +0100

    changed globals to the new width and height of the new grafics

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/cupertino.dart';
 
-Size backgroundSize = Size(1280,720);
-Offset orgelOffset = Offset(5, -20);
-Size orgelSize  =  Size(794,559);
+Size backgroundSize = Size(1980,1080);
+Offset orgelOffset = Offset(5, 5);
+Size orgelSize  =  Size(450,639);
 Offset crankOffset = Offset(550, 459);
-Size crankSize = Size(500,500);
+Size crankSize = Size(450,450);
 Offset topfOffset = Offset(100,100);
-Size topfSize = Size(100,500);
-Offset salutisOffset = Offset(100,100);
-Size salutisSize = Size(100,500);
+Size topfSize = Size(540,824);
+Size spendenOffset = Size(200,300);
+Offset maleSalutisOffset = Offset(100,0);
+Size maleSalutisSize = Size(300,904);
+Offset femaleSalutisOffset = Offset(100,0);
+Size femaleSalutisSize = Size(255,956);
