93fe94c49d339c16fe844e7e72aaba1ee187493a
paradoxon <blablabla!1865Atlassian>
2021-12-12 20:00:33 +0100

    use new donations class and salutists changed Messagesystem to ValueNotifier

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,15 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:orgel/About.dart';
 import 'package:orgel/organ.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:orgel/salutisten.dart';
 
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:orgel/the_crank.dart';
 import 'package:orgel/donation_button.dart';
+
+import 'donations.dart';
+import 'globals.dart';
 
 void main() {
   String? corps = Uri.base.queryParameters["custom1"]; //get the parameter for which Corps
@@ -40,15 +43,49 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  GlobalKey previewContainer = new GlobalKey();
-  GlobalKey dBKey  = new GlobalKey();
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
+class StatefulWrapper extends StatefulWidget {
+  final Function onInit;
+  final Widget child;
+  const StatefulWrapper({required this.onInit, required this.child});
+  @override
+  _StatefulWrapperState createState() => _StatefulWrapperState();
+}
+class _StatefulWrapperState extends State<StatefulWrapper> {
+  @override
+  void initState() {
+    if(widget.onInit != null) {
+      widget.onInit();
+    }
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    return RepaintBoundary(
+    return widget.child;
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  final ValueNotifier<double> _money = ValueNotifier<double>(0.0);
+  final ValueNotifier<int> _rotation = ValueNotifier<int>(0);
+
+  GlobalKey previewContainer = new GlobalKey();
+  GlobalKey<DonationButtonState> dBKey  = new GlobalKey();
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+
+  Future _init() async {
+    _rotation.addListener(() {
+      dBKey.currentState?.newMoney();
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return StatefulWrapper(
+        onInit: () {
+        _init().then((value) {});
+    },
+    child: RepaintBoundary(
         key: previewContainer,
         child: Scaffold(
             appBar: AppBar(
@@ -65,6 +102,7 @@ class MyHomePage extends StatelessWidget {
               )
             ),
             body: Container(
+                alignment: FractionalOffset(0.0,1.0),
                 decoration: BoxDecoration(
                   image:  DecorationImage(
                     image: AssetImage("assets/images/background.jpg"),
@@ -75,11 +113,29 @@ class MyHomePage extends StatelessWidget {
                 Stack(
                   fit: StackFit.expand,
                     clipBehavior: Clip.none,
+                    alignment: FractionalOffset(0.0,1.0),
                     children: <Widget>[
-                      Organ(1.0),
-                      //DonationButton(key: dBKey),
-                      TheCrank(donationButtonKey: dBKey),
+                      Salutisten(),
+                      FractionalTranslation(
+                          translation: topfOffset,
+                            child: Stack(
+                              alignment: FractionalOffset(0,0),
+                            children: <Widget>[
+                              Image.asset('assets/images/bucket.png',alignment: Alignment.bottomCenter,),
+                              Donations(money: _money,)
+                            ]
+                          )
+                      ),
+
+                      Organ(),
+                      TheCrank(rotation: _rotation,),
                       Positioned.fill(
+                          child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: DonationButton(key:dBKey,money: _money)
+                          )
+                      ),
+                      /*Positioned.fill(
                         child: Align(
                             alignment: Alignment.bottomCenter,
                             child: FloatingActionButton(
@@ -89,13 +145,14 @@ class MyHomePage extends StatelessWidget {
                                 child: Icon(Icons.share)
                             )
                         )
-                      )
+                      )*/
 
                     ],
                   ),
                 )
                 )
-            );
+            )
+    );
   }
 
   takeScreenShot() async {
@@ -124,7 +181,7 @@ class MyHomePage extends StatelessWidget {
         }
       }
       print("now we can share the screenshot");
-      Share.shareFiles(['$directory/screenshot.png'], text: 'Ich hab für die Heilsarmee georgelt - probiers auch mal! www.projectconceptor.de/orgeln');
+      Share.shareFiles(['$directory/screenshot.png'], text: 'Ich hab für die Heilsarmee georgelt!\n - probiers auch mal! www.heilsarmee.de/files/orgel-app/index.html');
       //Share.share("Ich hab für die Heilsarmee georgelt");
     } catch (e) {
       print('error: $e');
