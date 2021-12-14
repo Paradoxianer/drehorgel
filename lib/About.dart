853cb672d9733d497669ae50aa41910063598ca3
paradoxon <blablabla!1865Atlassian>
2021-12-14 10:35:49 +0100

    fixed About Dialog and added icon to assets to show it in the dialog

--- lib/About.dart
+++ lib/About.dart
@@ -11,13 +11,13 @@ class About {
         applicationName: "Orgeln",
         context: context,
         applicationVersion: '1.0.1',
-        applicationIcon: Icon(Icons.settings_applications),
+        applicationIcon: Image.asset('assets/images/ic_launcher.png'),
         children: <Widget>[
           RichText(
               text:
               TextSpan(
                   text: 'Ein App mit der man Drehorgeln kann.\nSimuliert wird das weihnacthliche Spendensammeln der Heilsarmee.\nAm Ende hat der Nutzer die Möglichkeit den erorgelten Betrag zu spenden.\n\n',
-                  // style: DefaultTextStyle.of(context).style,
+                  style: TextStyle(color: Colors.black),
                   children:  <TextSpan>[
                     TextSpan(text: "Idee und Umsetzung - Matthias Lindner\n\n"),
                     TextSpan(text: "Datenschutzerklärung - auf github.com\n",
@@ -33,12 +33,6 @@ class About {
                         },
                     ),
                     TextSpan(text:"===== DANK geht an =====\n\n"),
-                    TextSpan(text:"Hintergrund - "),
-                    TextSpan(text:"Pixabay\n",
-                      style: TextStyle(color: Colors.blue),
-                      recognizer: new TapGestureRecognizer()
-                        ..onTap = () { launch('https://pixabay.com/illustrations/christmas-background-landscape-4701783');
-                        },),
                     TextSpan(text:"Musik - Daniel Matzeit - "),
                     TextSpan(text:"Website\n",
                       style: TextStyle(color: Colors.blue),
@@ -47,7 +41,7 @@ class About {
                         },
                     ),
                     TextSpan(text:"Florian Walz - "),
-                    TextSpan(text:"Website\n",
+                    TextSpan(text:"Website\n\n",
                       style: TextStyle(color: Colors.blue),
                       recognizer: new TapGestureRecognizer()
                         ..onTap = () { launch('https://profis.check24.de/profil/tastenheld/aqgrra');
